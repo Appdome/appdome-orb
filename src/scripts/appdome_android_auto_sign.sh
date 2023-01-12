871ae3846d11503fdb9d187b39b059ace03e8c05
Idan Hauser <idanhauser@gmail.com>
2023-01-12 16:55:43 +0200

    ANDROID_AUTO_SIGNING - without google sign

--- src/scripts/appdome_android_auto_sign.sh
+++ src/scripts/appdome_android_auto_sign.sh
@@ -6,7 +6,7 @@ VAR="${SIGNOVERRIDES}"
 ls
 if [[ -n "$VAR" ]]; then
     echo "detected sign overrides"
-    if [ "${GOOGLEPLAYSIGN}" -eq 0 ]; then
+    if [ "${GOOGLEPLAYSIGN}" -eq 1 ]; then
         echo "google play sign"
         echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --sign_on_appdome --keystore files/"$(basename "$KEYSTORE")" --keystore_pass "${KEYSTORE_PASS}" --keystore_alias "${KEYSTORE_ALIAS}" --key_pass "${KEYSTORE_KEY_PASS}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
         python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --sign_on_appdome --keystore files/"$(basename "$KEYSTORE")" --keystore_pass "${KEYSTORE_PASS}" --keystore_alias "${KEYSTORE_ALIAS}" --key_pass "${KEYSTORE_KEY_PASS}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
@@ -17,7 +17,7 @@ if [[ -n "$VAR" ]]; then
     fi
 else
     echo "no sign overrides"
-    if [ "${GOOGLEPLAYSIGN}" -eq 0 ]; then
+    if [ "${GOOGLEPLAYSIGN}" -eq 1 ]; then
         echo "google play sign"
         echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --google_play_signing --sign_on_appdome --keystore files/"$(basename "$KEYSTORE")" --keystore_pass "${KEYSTORE_PASS}" --keystore_alias "${KEYSTORE_ALIAS}" --key_pass "${KEYSTORE_KEY_PASS}" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf 
         python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --google_play_signing --sign_on_appdome --keystore files/"$(basename "$KEYSTORE")" --keystore_pass "${KEYSTORE_PASS}" --keystore_alias "${KEYSTORE_ALIAS}" --key_pass "${KEYSTORE_KEY_PASS}" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
