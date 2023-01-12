9ee46d8b40e64c9ea4691ad42c71e3172dce2787
Idan Hauser <idanhauser@gmail.com>
2023-01-12 17:05:19 +0200

    ANDROID_PRIVATE_SIGNING - no google signing fixed scriptfiles

--- src/scripts/appdome_android_private_sign.sh
+++ src/scripts/appdome_android_private_sign.sh
@@ -7,7 +7,7 @@ VAR="${SIGNOVERRIDES}"
 ls
 if [[ -n "$VAR" ]]; then
     echo "detected sign overrides"
-    if [ "${GOOGLEPLAYSIGN}" -eq 0 ]; then
+    if [ "${GOOGLEPLAYSIGN}" -eq 1 ]; then
         echo "google play sign"
         echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint  "${FINGERPRINT}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
         python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint  "${FINGERPRINT}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
@@ -18,7 +18,7 @@ if [[ -n "$VAR" ]]; then
     fi
 else
     echo "no sign overrides"
-    if [ "${GOOGLEPLAYSIGN}" -eq 0 ]; then
+    if [ "${GOOGLEPLAYSIGN}" -eq 1 ]; then
         echo "google play sign"
         echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint "${FINGERPRINT}" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
         python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint "${FINGERPRINT}" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
