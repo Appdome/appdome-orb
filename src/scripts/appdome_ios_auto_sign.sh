773252baf24f61fc34b6d6e2a1baaa83701f0634
Idan Hauser <idanhauser@gmail.com>
2023-01-19 23:53:22 +0200

    Resolves @NF-XXXXX, Resolution Description: Testing Instructions: Reviewed By:

--- src/scripts/appdome_ios_auto_sign.sh
+++ src/scripts/appdome_ios_auto_sign.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+base64 --decode "${!P12_FILE}" > files/Cert.p12
+base64 --decode "${!PROVISIONING_PROFILES}" > files/provisioning_profiles.mobileprovision
+base64 --decode "${!ENTITLEMENTS}" > files/Entitlements.plist
+
 echo "Appdome iOS auto sign"
 mkdir output
 VAR="${SIGNOVERRIDES}"
 ls
 if [[ -n "$VAR" ]]; then
     echo "detected sign overrides"
-    echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --sign_on_appdome --keystore files/"$(basename "$KEYSTORE")" --keystore_pass "${!KEYSTORE_PASS}" --provisioning_profiles files/"$(basename "$PROVISIONING_PROFILES")" --entitlements files/"$(basename "$ENTITLEMENTS")" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
-    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --sign_on_appdome --keystore files/"$(basename "$KEYSTORE")" --keystore_pass "${!KEYSTORE_PASS}" --provisioning_profiles files/"$(basename "$PROVISIONING_PROFILES")" --entitlements files/"$(basename "$ENTITLEMENTS")" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
+    echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --sign_on_appdome --keystore files/Cert.p12 --keystore_pass "${!P12_PASS}" --provisioning_profiles files/provisioning_profiles.mobileprovision --entitlements files/Entitlements.plist --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
+    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --sign_on_appdome --keystore files/Cert.p12 --keystore_pass "${!P12_PASS}" --provisioning_profiles files/provisioning_profiles.mobileprovision --entitlements files/Entitlements.plist --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
 else
     echo "no sign overrides"
-    echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --sign_on_appdome --keystore files/"$(basename "$KEYSTORE")" --keystore_pass "${!KEYSTORE_PASS}" --provisioning_profiles files/"$(basename "$PROVISIONING_PROFILES")" --entitlements files/"$(basename "$ENTITLEMENTS")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
-    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --sign_on_appdome --keystore files/"$(basename "$KEYSTORE")" --keystore_pass "${!KEYSTORE_PASS}" --provisioning_profiles files/"$(basename "$PROVISIONING_PROFILES")" --entitlements files/"$(basename "$ENTITLEMENTS")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
+    echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --sign_on_appdome --keystore files/Cert.p12 --keystore_pass "${!P12_PASS}" --provisioning_profiles files/provisioning_profiles.mobileprovision --entitlements files/Entitlements.plist --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
+    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --sign_on_appdome --keystore files/Cert.p12 --keystore_pass "${!P12_PASS}" --provisioning_profiles files/provisioning_profiles.mobileprovision --entitlements files/Entitlements.plist --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
 fi
--- src/scripts/appdome_ios_autodev_private_sign.sh
+++ src/scripts/appdome_ios_autodev_private_sign.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+base64 --decode "${!PROVISIONING_PROFILES}" > files/provisioning_profiles.mobileprovision
+
 echo "Appdome iOS Auto-DEV private sign"
 mkdir output
 VAR="${SIGNOVERRIDES}"
 ls
 if [[ -n "$VAR" ]]; then
     echo "detected sign overrides"
-    echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --provisioning_profiles files/"$(basename "$PROVISIONING_PROFILES")" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
-    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --provisioning_profiles files/"$(basename "$PROVISIONING_PROFILES")" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
+    echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
+    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
 else
     echo "no sign overrides"
-    echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --provisioning_profiles files/"$(basename "$PROVISIONING_PROFILES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
-    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --provisioning_profiles files/"$(basename "$PROVISIONING_PROFILES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
+    echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
+    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
 fi
