96e4c443c0a33ef657514ae3d80bd16379e66c99
Idan Hauser <idanhauser@gmail.com>
2023-01-19 10:53:32 +0200

    Resolves @NF-XXXXX, Resolution Description: Testing Instructions: Reviewed By:

--- src/scripts/appdome_android_private_sign.sh
+++ src/scripts/appdome_android_private_sign.sh
@@ -9,10 +9,7 @@ APPDOME_API_KEY="$(eval printf '%s' "$APPDOME_API_KEY")"
 echo token = APPDOME_API_KEY
 echo token = "${!APPDOME_API_KEY}"
 echo token = "${APPDOME_API_KEY}"
-echo token = ${APPDOME_API_KEY}
 echo token = ${!APPDOME_API_KEY}
-echo token = "$APPDOME_API_KEY"
-echo token = $APPDOME_API_KEY
 
 if [[ -n "$VAR" ]]; then
     echo "detected sign overrides"
