fb6d512484be4990c9f0c4f64c698f80d8d61412
Idan Hauser <idanhauser@gmail.com>
2023-01-20 10:45:02 +0200

    Resolves @NF-XXXXX, Resolution Description: Testing Instructions: Reviewed By:

--- src/scripts/appdome_ios_autodev_private_sign.sh
+++ src/scripts/appdome_ios_autodev_private_sign.sh
@@ -13,7 +13,5 @@ if [[ -n "$VAR" ]]; then
 else
     echo "no sign overrides"
     echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
-#  python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
-    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
-
+    python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${FUSIONSET}" --app files/"$(basename "$APPFILE")" --auto_dev_private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
 fi
