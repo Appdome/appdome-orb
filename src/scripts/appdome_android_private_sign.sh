f4fd07e1b0c617e0c89eb39ea6e3ce3833174942
avie <avie@appdome.com>
2023-02-01 17:44:01 +0200

    test

--- src/scripts/appdome_android_private_sign.sh
+++ src/scripts/appdome_android_private_sign.sh
@@ -10,6 +10,11 @@ if [[ -n "$VAR" ]]; then
     echo "detected sign overrides"
     if [ "${GOOGLEPLAYSIGN}" -eq 1 ]; then
         echo "google play sign"
+        echo "${!APPDOME_API_KEY}"
+        echo ${!APPDOME_API_KEY}
+        echo ${APPDOME_API_KEY}
+        echo $APPDOME_API_KEY
+        echo "$APPDOME_API_KEY"
         echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint  "${!FINGERPRINT}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
         python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint  "${!FINGERPRINT}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
     else
