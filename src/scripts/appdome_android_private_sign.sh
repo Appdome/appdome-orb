3cf5c538557fcc8fb1d3ecc3783be0376b7b1055
avie <avie@appdome.com>
2023-02-01 17:48:30 +0200

    test

--- src/scripts/appdome_android_private_sign.sh
+++ src/scripts/appdome_android_private_sign.sh
@@ -6,13 +6,14 @@ env
 mkdir output
 VAR="${SIGNOVERRIDES}"
 ls
+echo "#######"
+echo "${!APPDOME_API_KEY}"
+echo "${APPDOME_API_KEY}"
+echo "$APPDOME_API_KEY"
 if [[ -n "$VAR" ]]; then
     echo "detected sign overrides"
     if [ "${GOOGLEPLAYSIGN}" -eq 1 ]; then
         echo "google play sign"
-        echo "${!APPDOME_API_KEY}"
-        echo "${APPDOME_API_KEY}"
-        echo "$APPDOME_API_KEY"
         echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint  "${!FINGERPRINT}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
         python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint  "${!FINGERPRINT}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
     else
