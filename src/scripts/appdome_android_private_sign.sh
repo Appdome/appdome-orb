5138f03ebd319ef4a61bd1f06ba8f7c0b61d958f
Idan Hauser <idanhauser@gmail.com>
2023-02-01 12:25:43 +0200

    fixed syntax errors. Resolves @NF-45114,

--- src/scripts/appdome_android_private_sign.sh
+++ src/scripts/appdome_android_private_sign.sh
@@ -16,6 +16,7 @@ if [[ -n "$VAR" ]]; then
             echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint  "${!FINGERPRINT}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
             python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint  "${!FINGERPRINT}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
         fi
+    fi
     else
         echo "no google play sign"
         if [[ -n "${TEAMID}" ]]; then
@@ -24,7 +25,7 @@ if [[ -n "$VAR" ]]; then
         else
             echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint  "${!FINGERPRINT}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
             python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint  "${!FINGERPRINT}" --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
-
+        fi
     fi
 else
     echo "no sign overrides"
@@ -36,6 +37,7 @@ else
         else
             echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint "${!FINGERPRINT}" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
             python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint "${!FINGERPRINT}" --google_play_signing --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
+        fi
     else
         echo "no google play sign"
         if [[ -n "${TEAMID}" ]]; then
@@ -44,5 +46,6 @@ else
         else
             echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint "${!FINGERPRINT}" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
             python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --signing_fingerprint "${!FINGERPRINT}" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf
+        fi
     fi
 fi
