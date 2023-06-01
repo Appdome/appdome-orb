3caed19543c528dde30056f7b0286b22e4c1ab0a
Idan Hauser <idanhauser@gmail.com>
2023-06-01 14:20:34 +0300

    Resolves @NF-XXXXX, Resolution Description: Testing Instructions: Reviewed By:

--- src/scripts/download_files.sh
+++ src/scripts/download_files.sh
@@ -6,6 +6,7 @@ echo "Downloading android file"
 if [[ "${APPFILE}" == http* ]]; then
     wget "${APPFILE}" -O files/"$(basename "$APPFILE")"
 else
+    ls -la ${APPFILE}"
     cp "${APPFILE}" files/
 fi
 
