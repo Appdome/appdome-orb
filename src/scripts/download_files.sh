9627a787b34ea2430b3f648a68aa9d591f90bdc5
Idan Hauser <idanhauser@gmail.com>
2023-06-01 11:29:54 +0300

    check

--- src/scripts/download_files.sh
+++ src/scripts/download_files.sh
@@ -3,6 +3,7 @@
 
 mkdir files
 echo "export ARTIFACTS_LOCATION=\"/home/circleci/appdome_workspace/output\"" >> "$BASH_ENV"
+echo "ARTIFACTS_LOCATION is set to: $ARTIFACTS_LOCATION"
 echo "Downloading android file"
 wget "${APPFILE}" -O files/"$(basename "$APPFILE")"
 if [[ -n "${SIGNOVERRIDES}" ]]; then
