a3c2e19cfd4fed97958e1ca36a2c19809dfc5d7d
Idan Hauser <idanhauser@gmail.com>
2023-06-01 15:55:35 +0300

    Chanegd folders to add a prefix of appdome

--- src/scripts/download_files.sh
+++ src/scripts/download_files.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 
-mkdir -p files
+mkdir -p appdome_files
 echo "Downloading android file"
 if [[ "${APPFILE}" == http* ]]; then
-    wget "${APPFILE}" -O files/"$(basename "$APPFILE")"
+    wget "${APPFILE}" -O appdome_files/"$(basename "$APPFILE")"
 else
     ls -la "${APPFILE}"
-    cp "${APPFILE}" files/
+    cp "${APPFILE}" appdome_files/
 fi
 
 if [[ -n "${SIGNOVERRIDES}" ]]; then
     echo "Downloading sign overrides"
     echo "${SIGNOVERRIDES}"
-    wget "${SIGNOVERRIDES}" -O files/"$(basename "$SIGNOVERRIDES")"
+    wget "${SIGNOVERRIDES}" -O appdome_files/"$(basename "$SIGNOVERRIDES")"
 fi
-ls files
+ls appdome_files
 
 
