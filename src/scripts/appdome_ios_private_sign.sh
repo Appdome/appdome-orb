68c56d4bba509c9a21f71c9372362b3ae4c945d9
Idan Hauser <idanhauser@gmail.com>
2023-06-13 12:55:15 +0300

    Changing the ios private sign command

--- src/scripts/appdome_ios_private_sign.sh
+++ src/scripts/appdome_ios_private_sign.sh
@@ -6,32 +6,25 @@ ls files
 mkdir output
 VAR="${SIGNOVERRIDES}"
 
-if [ "${BUILD_WITH_LOGS}" -eq 1 ]; then
-  BUILD_LOGS="--diagnostic_logs"
-else
-  BUILD_LOGS=""
-fi
-echo BUILD_WITH_LOGS = "${BUILD_WITH_LOGS}"
-echo build_logs = "$BUILD_LOGS"
-
-ls
-
 if [[ -n "$VAR" ]]; then
     echo "detected sign overrides"
     if [[ -n "${TEAMID}" ]]; then
-        echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" -t "${TEAMID}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf "$BUILD_LOGS"
-        python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}"  -t "${TEAMID}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf "$BUILD_LOGS"
+        command="python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key ${!APPDOME_API_KEY} -t ${TEAMID} --fusion_set_id ${!FUSIONSET} --app files/$(basename "$APPFILE") --private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --sign_overrides files/$(basename "$SIGNOVERRIDES") --output ./output/${OUTPUT} --certificate_output ./output/certificate.pdf"
     else
-        echo python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf "$BUILD_LOGS"
-        python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --sign_overrides files/"$(basename "$SIGNOVERRIDES")" --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf "$BUILD_LOGS"
+        command="python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key ${!APPDOME_API_KEY} --fusion_set_id ${!FUSIONSET} --app files/$(basename "$APPFILE") --private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --sign_overrides files/$(basename "$SIGNOVERRIDES") --output ./output/${OUTPUT} --certificate_output ./output/certificate.pdf"
     fi
 else
     echo "no sign overrides"
     if [[ -n "${TEAMID}" ]]; then
-        echo on3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" -t "${TEAMID}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf "$BUILD_LOGS"
-        python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" -t "${TEAMID}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf "$BUILD_LOGS"
+        command="python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key ${!APPDOME_API_KEY} -t ${TEAMID} --fusion_set_id ${!FUSIONSET} --app files/$(basename "$APPFILE") --private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --output ./output/${OUTPUT} --certificate_output ./output/certificate.pdf"
     else   
-        echo on3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" -t "${TEAMID}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf "$BUILD_LOGS"
-        python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key "${!APPDOME_API_KEY}" -t "${TEAMID}" --fusion_set_id "${!FUSIONSET}" --app files/"$(basename "$APPFILE")" --private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --output ./output/"${OUTPUT}" --certificate_output ./output/certificate.pdf "$BUILD_LOGS"
+        command="python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key ${!APPDOME_API_KEY} -t ${TEAMID} --fusion_set_id ${!FUSIONSET} --app files/$(basename "$APPFILE") --private_signing --provisioning_profiles files/provisioning_profiles.mobileprovision --output ./output/${OUTPUT} --certificate_output ./output/certificate.pdf"
     fi  
 fi
+
+if [[ -n "${BUILD_LOGS}" ]]; then
+    command+=" --diagnostic_logs"
+fi
+
+echo $command
+eval $command
