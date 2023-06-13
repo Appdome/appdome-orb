c5858087b3a5b8cd1f675a85a73e02f487850aec
Idan Hauser <idanhauser@gmail.com>
2023-06-13 14:09:42 +0300

    fix

--- src/scripts/appdome_android_auto_sign.sh
+++ src/scripts/appdome_android_auto_sign.sh
@@ -20,7 +20,7 @@ if [[ -n "$VAR" ]]; then
         if [[ -n "${TEAMID}" ]]; then
             command="python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key ${!APPDOME_API_KEY} -t ${TEAMID} --fusion_set_id ${!FUSIONSET} --app files/$(basename "$APPFILE") --sign_on_appdome --keystore files/keystore.keystore --keystore_pass ${!KEYSTORE_PASS} --keystore_alias ${!KEYSTORE_ALIAS_ENV} --key_pass ${!KEYSTORE_KEY_PASS} --sign_overrides files/$(basename "$SIGNOVERRIDES") --output ./output/${OUTPUT} --certificate_output ./output/certificate.pdf"
         else
-            command="python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key ${!APPDOME_API_KEY} --fusion_set_id ${!FUSIONSET} --app files/$(basename "$APPFILE") --sign_on_appdome --keystore files/keystore.keystore --keystore_pass ${!KEYSTORE_PASS} --keystore_alias ${!KEYSTORE_ALIAS_ENV} --key_pass ${!KEYSTORE_KEY_PASS} --sign_overrides files/$(basename "$SIGNOVERRIDES") --output ./output/"{OUTPUT} --certificate_output ./output/certificate.pdf"
+            command="python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key ${!APPDOME_API_KEY} --fusion_set_id ${!FUSIONSET} --app files/$(basename "$APPFILE") --sign_on_appdome --keystore files/keystore.keystore --keystore_pass ${!KEYSTORE_PASS} --keystore_alias ${!KEYSTORE_ALIAS_ENV} --key_pass ${!KEYSTORE_KEY_PASS} --sign_overrides files/$(basename "$SIGNOVERRIDES") --output ./output/{OUTPUT} --certificate_output ./output/certificate.pdf"
         fi
     fi
 else
