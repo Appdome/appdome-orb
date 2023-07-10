3edb80a72789f32bc7652329b4c687cbb0190945
Idan Hauser <idanhauser@gmail.com>
2023-07-10 17:33:15 +0300

    fix and test

--- src/scripts/appdome_ios_autodev_private_sign.sh
+++ src/scripts/appdome_ios_autodev_private_sign.sh
@@ -28,10 +28,6 @@ if [ "${BUILD_WITH_LOGS}" -eq 1 ]; then
     command+=" --diagnostic_logs"
 fi
 
-if [[ -n "${SECOND_OUTPUT}" ]]; then
-    command+=" --second_output ./appdome_outputs/${SECOND_OUTPUT}"
-fi
-
 if [[ -n "${BUILD_TO_TEST}" && "${BUILD_TO_TEST}" != "NONE" ]]; then
     command+=" --build_to_test_vendor ${BUILD_TO_TEST,,}"
 fi
