db574c7d9c65858d0c49ef6065d8fc3055b2418f
Idan Hauser <idanhauser@gmail.com>
2023-10-22 18:06:21 +0300

    Resolves @NF-XXXXX, Resolution Description: Testing Instructions: Reviewed By:

--- src/scripts/install_requirements.sh
+++ src/scripts/install_requirements.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ls "$ROOT_DIR/appdome_workspace/"
+ls "$ROOT_DIR/appdome_workspace/appdome-api-python/"
 pip3 install -r "$ROOT_DIR/appdome_workspace/appdome-api-python/requirements.txt"
 sudo apt install wget
