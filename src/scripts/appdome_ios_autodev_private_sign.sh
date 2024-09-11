a75ad0e45a2c89e8f4a2b28f40f9b5420d5bac21
Idan Hauser <idanh@appdome.com>
2024-09-11 18:09:19 +0300

    ios autodev private entitlements are not required

--- src/scripts/appdome_ios_autodev_private_sign.sh
+++ src/scripts/appdome_ios_autodev_private_sign.sh
@@ -3,32 +3,30 @@ echo "Appdome iOS Auto-DEV private sign"
 echo -n "${!PROVISIONING_PROFILES}" | base64 -d > appdome_files/provisioning_profiles.mobileprovision
 echo -n "${!ENTITLEMENTS}" | base64 -d > appdome_files/Entitlements.plist
 ls appdome_files
-mkdir appdome_outputs
+mkdir -p appdome_outputs
 VAR="${SIGNOVERRIDES}"
 
 basename=$(basename "$OUTPUT")
-
 export OUTPUT="${basename}.sh"
-
 
 echo "Output file name: ${OUTPUT}"
 
-
 ls
+entitlements_arg=""
+if [[ -s appdome_files/Entitlements.plist ]]; then
+    entitlements_arg="--entitlements appdome_files/Entitlements.plist"
+fi
+
 if [[ -n "$VAR" ]]; then
     echo "detected sign overrides"
-    if [[ -n "${TEAMID}" ]]; then
-        command="python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key ${!APPDOME_API_KEY} -t ${TEAMID} --fusion_set_id ${!FUSIONSET} --app appdome_files/$(basename "$APPFILE") --auto_dev_private_signing --provisioning_profiles appdome_files/provisioning_profiles.mobileprovision --entitlements appdome_files/Entitlements.plist --sign_overrides appdome_files/$(basename "$SIGNOVERRIDES") --output ./appdome_outputs/${OUTPUT} --certificate_output ./appdome_outputs/certificate.pdf"
-    else
-        command="python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key ${!APPDOME_API_KEY} --fusion_set_id ${!FUSIONSET} --app appdome_files/$(basename "$APPFILE") --auto_dev_private_signing --provisioning_profiles appdome_files/provisioning_profiles.mobileprovision --entitlements appdome_files/Entitlements.plist --sign_overrides appdome_files/$(basename "$SIGNOVERRIDES") --output ./appdome_outputs/${OUTPUT} --certificate_output ./appdome_outputs/certificate.pdf"
-    fi
+    command="python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key ${!APPDOME_API_KEY} --fusion_set_id ${!FUSIONSET} --app appdome_files/$(basename "$APPFILE") --auto_dev_private_signing --provisioning_profiles appdome_files/provisioning_profiles.mobileprovision ${entitlements_arg} --sign_overrides appdome_files/$(basename "$SIGNOVERRIDES") --output ./appdome_outputs/${OUTPUT} --certificate_output ./appdome_outputs/certificate.pdf"
 else
     echo "no sign overrides"
-    if [[ -n "${TEAMID}" ]]; then
-        command="python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key ${!APPDOME_API_KEY} -t ${TEAMID} --fusion_set_id ${!FUSIONSET} --app appdome_files/$(basename "$APPFILE") --auto_dev_private_signing --provisioning_profiles appdome_files/provisioning_profiles.mobileprovision --entitlements appdome_files/Entitlements.plist --output ./appdome_outputs/${OUTPUT} --certificate_output ./appdome_outputs/certificate.pdf"
-    else
-        command="python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key ${!APPDOME_API_KEY} --fusion_set_id ${!FUSIONSET} --app appdome_files/$(basename "$APPFILE") --auto_dev_private_signing --provisioning_profiles appdome_files/provisioning_profiles.mobileprovision --entitlements appdome_files/Entitlements.plist --output ./appdome_outputs/${OUTPUT} --certificate_output ./appdome_outputs/certificate.pdf"
-    fi
+    command="python3 ./appdome-api-python/appdome-api-python/appdome_api.py --api_key ${!APPDOME_API_KEY} --fusion_set_id ${!FUSIONSET} --app appdome_files/$(basename "$APPFILE") --auto_dev_private_signing --provisioning_profiles appdome_files/provisioning_profiles.mobileprovision ${entitlements_arg} --output ./appdome_outputs/${OUTPUT} --certificate_output ./appdome_outputs/certificate.pdf"
+fi
+
+if [[ -n "${TEAMID}" ]]; then
+    command+=" -t ${TEAMID}"
 fi
 
 if [ "${BUILD_WITH_LOGS}" -eq 1 ]; then
